8bf45ec97447df89ada7997754297a99e4399b4f
zhushman <zhushman.m.s@mail.ru>
2022-02-22 17:31:02 +0300

    add lint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,9 +3,9 @@ import { Command } from 'commander';
 
 const program = new Command();
 program
-    .description('Compares two configuration files and shows a difference.')
-    .version('0.0.1')
-    .argument('<filepath1> <filepath2>')
-    .option('-f, --format <type>', 'output format')
-  
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.0.1')
+  .argument('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
+
 program.parse();
