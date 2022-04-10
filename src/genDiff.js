054dd105545394adb28f007fdb75a40b5c5f0fbb
zhushman <zhushman.m.s@mail.ru>
2022-04-10 21:35:26 +0400

    add ast

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,33 +1,24 @@
-import { readFileSync } from 'fs';
 import _ from 'lodash';
-import path from 'path';
-import parse from './parsers.js';
 
-const genDiff = (data1, data2) => {
-  const keys1 = _.keys(data1);
-  const keys2 = _.keys(data2);
-  const keys = _.sortBy(_.union(keys1, keys2));
-  const dataDiff = ['{', ...keys
-    .map((key) => {
-      if (!_.has(data1, key)) {
-        return [`  + ${key}: ${data2[key]}`];
-      } if (!_.has(data2, key)) {
-        return [`  - ${key}: ${data1[key]}`];
-      } if (data1[key] !== data2[key]) {
-        return [`  - ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`];
-      } return [`    ${key}: ${data2[key]}`];
-    }, []), '}'].join('\n');
-  return dataDiff;
+const ast = (obj1, obj2) => {
+  const keys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));  
+  const result = keys.map((key)=> {
+    if (!_.has(obj2, key)) {
+      return {key, state: 'deleted', value: obj1[key]};
+    }
+    if (!_.has(obj1, key)) {
+      return {key, state: 'added', value: obj2[key]}
+    }
+    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
+      return {key, state: 'inner node', children: ast(obj1[key], obj2[key]) };
+    }
+    if (obj1[key] !== obj2[key]) {
+      return {key, state: 'changed', obj1Value: obj1[key], obj2Value: obj2[key]}
+    }
+    return {key, state: 'unchanged', value: obj2[key]}
+    });
+
+  return result;
 };
 
-export default (filepath1, filepath2) => {
-  const pathFirstFile = path.resolve(process.cwd(), filepath1);
-  const pathSecondFile = path.resolve(process.cwd(), filepath2);
-  const dataFirstFile = readFileSync(pathFirstFile, 'utf-8');
-  const dataSecondFile = readFileSync(pathSecondFile, 'utf-8');
-  const extnameFirstFile = path.extname(pathFirstFile).substring(1);
-  const extnameSecondFile = path.extname(pathSecondFile).substring(1);
-  const parsingDataFirstFile = parse(dataFirstFile, extnameFirstFile);
-  const parsingDataSecondFile = parse(dataSecondFile, extnameSecondFile);
-  return genDiff(parsingDataFirstFile, parsingDataSecondFile);
-};
+export default ast;
