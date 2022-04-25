9c506a695938e84c37cc065b7718dbb5dd7693e2
zero <zhushman.m.s@mail.ru>
2022-04-25 14:04:33 +0400

    fix bild path #2

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ export default (filepath1, filepath2, formatName = 'stylish') => {
   const pathSecondFile = path.resolve(process.cwd(), filepath2);
   const dataFirstFile = readFileSync(pathFirstFile, 'utf-8');
   const dataSecondFile = readFileSync(pathSecondFile, 'utf-8');
-  const extnameFirstFile = path.extname(pathFirstFile).substring(1);
-  const extnameSecondFile = path.extname(pathSecondFile).substring(1);
+  const extnameFirstFile = path.extname(path.basename(pathFirstFile)).substring(1);
+  const extnameSecondFile = path.extname(path.basename(pathSecondFile)).substring(1);
   const parsingDataFirstFile = parse(dataFirstFile, extnameFirstFile);
   const parsingDataSecondFile = parse(dataSecondFile, extnameSecondFile);
   const astDiff = genDiff(parsingDataFirstFile, parsingDataSecondFile);
