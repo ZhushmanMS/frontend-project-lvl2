f3abfc9853ed49b99b8b09f533d14236005e2566
zero <zhushman.m.s@mail.ru>
2022-04-25 13:07:38 +0400

    edit path

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import genDiff from './genDiff.js';
 import format from './formatters/index.js';
 
 export default (filepath1, filepath2, formatName = 'stylish') => {
-  const pathFirstFile = path.resolve(process.cwd(), filepath1);
-  const pathSecondFile = path.resolve(process.cwd(), filepath2);
-  const dataFirstFile = readFileSync(pathFirstFile, 'utf-8');
-  const dataSecondFile = readFileSync(pathSecondFile, 'utf-8');
-  const extnameFirstFile = path.extname(pathFirstFile).substring(1);
-  const extnameSecondFile = path.extname(pathSecondFile).substring(1);
+  // const pathFirstFile = path.resolve(process.cwd(), filepath1);
+  // const pathSecondFile = path.resolve(process.cwd(), filepath2);
+  const dataFirstFile = readFileSync(filepath1, 'utf-8');
+  const dataSecondFile = readFileSync(filepath2, 'utf-8');
+  const extnameFirstFile = path.extname(filepath1).substring(1);
+  const extnameSecondFile = path.extname(filepath2).substring(1);
   const parsingDataFirstFile = parse(dataFirstFile, extnameFirstFile);
   const parsingDataSecondFile = parse(dataSecondFile, extnameSecondFile);
   const astDiff = genDiff(parsingDataFirstFile, parsingDataSecondFile);
