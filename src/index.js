59913d80400e0853248b36b757c241f31e413257
zero <zhushman.m.s@mail.ru>
2022-04-25 16:08:29 +0400

    fix bild path #3

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import format from './formatters/index.js';
 
 export default (filepath1, filepath2, formatName = 'stylish') => {
   const pathFirstFile = path.resolve(process.cwd(), filepath1);
+  console.log(pathFirstFile);
   const pathSecondFile = path.resolve(process.cwd(), filepath2);
   const dataFirstFile = readFileSync(pathFirstFile, 'utf-8');
   const dataSecondFile = readFileSync(pathSecondFile, 'utf-8');
-  const extnameFirstFile = path.extname(path.basename(pathFirstFile)).substring(1);
-  const extnameSecondFile = path.extname(path.basename(pathSecondFile)).substring(1);
+  const extnameFirstFile = path.extname(pathFirstFile).slice(1);
+  console.log(`\n${extnameFirstFile}`);
+  const extnameSecondFile = path.extname(pathSecondFile).slice(1);
   const parsingDataFirstFile = parse(dataFirstFile, extnameFirstFile);
   const parsingDataSecondFile = parse(dataSecondFile, extnameSecondFile);
   const astDiff = genDiff(parsingDataFirstFile, parsingDataSecondFile);
