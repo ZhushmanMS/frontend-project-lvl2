faf2ceb4fdb2f482adbdac71663dd16b5118e4c3
zero <zhushman.m.s@mail.ru>
2022-04-26 08:58:41 +0400

    fix entry point

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,9 +6,10 @@ const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
-  .arguments('<filepath1> <filepath2>')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action((filePath1, filePath2, options) => {
-    console.log(genDiff(filePath1, filePath2, options.format));
+  .action((filepath1, filepath2) => {
+    console.log(genDiff(filepath1, filepath2, program.opts().format));
   })
   .parse(process.argv);
--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import gendiff from './src/genDiff.js';
+import gendiff from './src/index.js';
 
 export default gendiff;
