4d9c73f2affa6548265c2909301938444ee34436
zhushman <zhushman.m.s@mail.ru>
2022-04-29 13:08:18 +0400

    fix parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,15 +1,14 @@
 import yaml from 'js-yaml';
-import _ from 'lodash';
-
-const parsers = {
-  json: JSON.parse,
-  yaml: yaml.load,
-  yml: yaml.load,
-};
 
 export default (fileData, dataFormat) => {
-  if (!_.has(parsers, dataFormat)) {
-    throw new Error(`file extension ${dataFormat} invalid!`);
+  switch (dataFormat) {
+    case 'json':
+      return JSON.parse(fileData);
+    case 'yaml':
+      return yaml.load(fileData);
+    case 'yml':
+      return yaml.load(fileData);
+    default:
+      throw new Error(`file extension ${dataFormat} invalid!`);
   }
-  return parsers[dataFormat](fileData);
 };
